fb77fc59779deea71140f8d2ad0f4ca33a51fa58
Muhammad Saman <samanmuhammad077@gmail.com>
2023-09-02 14:42:10 +0800

    modified:   src/components/Profile.jsx 	modified:   src/App.css 	modified:   src/components/SosmedList.jsx 	deleted:    src/components/Sosmend.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import foto from "../assets/foto.png";
 export default function Profile() {
   return (
     <>
-      <img src={foto} alt="" className="mx-auto rounded-full mt-10" />
+      <img src={foto} alt="" className="mx-auto rounded-full mt-8" />
       <h1 className="font-jakarta font-extrabold text-2xl mt-8 mb-3">
         Muhamad Saman
       </h1>
